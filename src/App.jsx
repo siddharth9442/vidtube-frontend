69cc3187a1aac50e10d0cb36ec4e6ed97e11f418
siddharth9442 <siddharthkhubikar9442@gmail.com>
2024-03-20 14:55:53 +0530

    update 3 files and delete 5 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { Outlet } from 'react-router-dom'
 import './App.css'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <div>
-        <Header />
-        <main>
-        {/* <Outlet /> */}
-        </main>
-        {/* <Footer /> */}
-      </div>
+      <h1>hey there, it's just begining..!</h1>
     </>
   )
 }
